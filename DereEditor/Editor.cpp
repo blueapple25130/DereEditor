eb7778ac12a3e986e86ae76accbc8d8c6c6f2aff
rare <work@DESKTOP-Q6BN0UU>
2017-07-27 02:25:01 +0900

    帯の接続判定と小節の操作を実装

--- DereEditor/Editor.cpp
+++ DereEditor/Editor.cpp
@@ -2,6 +2,7 @@
 
 void Editor::updateNoteState() {
 	m_notes.clear();
+	m_playableNotes.clear();
 	double plevNoteTick = 0;
 	double plevNoteTime = 0;//Offset
 	double currentTempo = 160;//StandardTempo
@@ -15,11 +16,12 @@ void Editor::updateNoteState() {
 			plevNoteTime = note->Time;
 			if (typeid(*note) == typeid(PlayableNote))
 			{
-				note->setPosition(getLaneOffset() + dynamic_cast<PlayableNote*>(note.get())->Lane * m_LaneWidth, static_cast<int>(-currentNoteTick / 192 * getBeatPerHeight()));
+				note->setPosition(getLaneOffset() + std::dynamic_pointer_cast<PlayableNote>(note)->Lane * m_LaneWidth, static_cast<int>(-currentNoteTick / 192 * getBeatPerHeight()));
+				m_playableNotes.push_back(std::dynamic_pointer_cast<PlayableNote>(note));
 			}
 			else if (typeid(*note) == typeid(ChangeTempo)) {
 				note->setPosition(512, static_cast<int>(-currentNoteTick / 192 * getBeatPerHeight()));
-				currentTempo = dynamic_cast<ChangeTempo*>(note.get())->getTempo();
+				currentTempo = std::dynamic_pointer_cast<ChangeTempo>(note)->getTempo();
 				//TempoChangeLeap.Add(note);
 			}
 		}
@@ -29,9 +31,61 @@ void Editor::updateNoteState() {
 		currentMeasureRealTick += measure->getRealTick();
 	}
 
-	// TODO:ノート間の接続を実装
+	std::sort(m_notes.begin(), m_notes.end(), [](const std::shared_ptr<Note>& a, const std::shared_ptr<Note>& b)
+	{
+		return a->Time < b->Time;
+	});
+
+
+	std::sort(m_playableNotes.begin(), m_playableNotes.end(), [](const std::shared_ptr<PlayableNote>& a, const std::shared_ptr<PlayableNote>& b)
+	{
+		return a->Time < b->Time;
+	});
+
 	// TODO:テンポを考慮したスクロールを実装
 
+	for (auto currentNote = m_playableNotes.begin(); currentNote != m_playableNotes.end(); ++currentNote) {
+		(*currentNote)->ConnectNote = nullptr;
+		switch ((*currentNote)->getNoteType())
+		{
+		case NoteType::LFlick:
+		case NoteType::RFlick:
+		{
+			auto connectNote = std::find_if(m_playableNotes.begin(), m_playableNotes.end(), [&](std::shared_ptr<PlayableNote> x) {return x->Time > (*currentNote)->Time &&  x->Channel == (*currentNote)->Channel && x->Time - (*currentNote)->Time < 1.0; });
+			if (connectNote != m_playableNotes.end()) {
+				if ((*connectNote)->Channel % 4 < 2) {
+					if ((*connectNote)->Lane < (*currentNote)->Lane && (*currentNote)->getNoteType() == NoteType::LFlick && ((*connectNote)->getNoteType() == NoteType::LFlick || (*connectNote)->getNoteType() == NoteType::RFlick))
+					{
+						(*currentNote)->ConnectNote = (*connectNote);
+					}
+					else if ((*connectNote)->Lane > (*currentNote)->Lane && (*currentNote)->getNoteType() == NoteType::RFlick && ((*connectNote)->getNoteType() == NoteType::LFlick || (*connectNote)->getNoteType() == NoteType::RFlick))
+					{
+						(*currentNote)->ConnectNote = (*connectNote);
+
+					}
+				}
+				else if((*connectNote)->getNoteType() == NoteType::LFlick || (*connectNote)->getNoteType() == NoteType::RFlick){
+					(*currentNote)->ConnectNote = (*connectNote);
+				}
+			}
+		}
+		break;
+		case NoteType::Long:
+		{
+			auto connectNote = std::find_if(m_playableNotes.begin(), m_playableNotes.end(), [&](std::shared_ptr<PlayableNote> x) {return x->Time > (*currentNote)->Time && x->Lane == (*currentNote)->Lane; });
+			if (connectNote != m_playableNotes.end())
+				(*currentNote)->ConnectNote = (*connectNote);
+		}
+		break;
+		case NoteType::Slide:
+		{
+			auto connectNote = std::find_if(m_playableNotes.begin(), m_playableNotes.end(), [&](std::shared_ptr<PlayableNote> x) {return x->Time > (*currentNote)->Time && x->Channel == (*currentNote)->Channel; });
+			if (connectNote != m_playableNotes.end())
+				(*currentNote)->ConnectNote = (*connectNote);
+		}
+		break;
+		}
+	}
 }
 
 double Editor::getBeatPerHeight() {
@@ -44,7 +98,7 @@ int Editor::getLaneOffset() {
 	return m_LaneOffset;
 }
 
-void Editor::setOrRemoveNote(std::vector<std::shared_ptr<Note>> &notes, int tick, int lane) {
+void Editor::setOrRemoveNote(std::vector<std::shared_ptr<Note>> &notes, int tick, int lane, NoteType type) {
 	for (auto it = notes.begin(); it != notes.end();) {
 		if ((*it)->Tick == tick && (*it)->Lane == lane) {
 			notes.erase(it);
@@ -58,8 +112,70 @@ void Editor::setOrRemoveNote(std::vector<std::shared_ptr<Note>> &notes, int tick
 		notes.push_back(std::make_shared<ChangeTempo>(tick, Parse<double>(m_gui.textField(L"tempo").text)));
 	}
 	else {
-		notes.push_back(std::make_shared<PlayableNote>(tick, lane, NoteType::Tap));
+		notes.push_back(std::make_shared<PlayableNote>(tick, lane, m_channel, type));
 	}
+}
+
+void Editor::changeZoomRatio() {
+	m_zoom = m_gui.slider(L"zoom").value;
+	m_gui.text(L"zoom-ratio").text = Format(static_cast<int>(m_zoom * 100), L"%");
+	updateNoteState();
+}
+
+void Editor::addMeasure() {
+	if (m_measures.size() == MEASURE_MAX) {
+		MessageBox::Show(L"これ以上小節を追加出来ません。");
+		return;
+	}
+
+	m_measures.insert(m_measures.begin() + m_selectedMeasure, std::make_shared<Measure>());
+	++m_selectedMeasure;
+	updateNoteState();
+}
+
+void Editor::removeMeasure() {
+	if (m_measures.size() == MEASURE_MIN) {
+		MessageBox::Show(L"小節を全て削除することは出来ません。");
+		return;
+	}
+
+	if (m_measures[m_selectedMeasure]->getNotes().size() > 0) {
+		if (MessageBox::Show(L"ノートが含まれています。小節を削除しますか？", MessageBoxStyle::YesNo) == MessageBoxCommand::Yes) {
+			m_measures.erase(m_measures.begin() + m_selectedMeasure--);
+			if (m_measures.size() == m_selectedMeasure) {
+				--m_selectedMeasure;
+			}
+			updateNoteState();
+		}
+	}
+	else {
+		m_measures.erase(m_measures.begin() + m_selectedMeasure);
+		if (m_measures.size() == m_selectedMeasure) {
+			--m_selectedMeasure;
+		}
+		updateNoteState();
+	}
+}
+
+void Editor::setMeasureLength() {
+	double length = Parse<double>(m_gui.textField(L"measure-length").text);
+	if (length <= 0) {
+		MessageBox::Show(L"小節の長さは0以下に設定出来ません。");
+		return;
+	}
+
+	//TODO:縮小範囲にノートが含まれている場合の処理を実装
+	m_measures[m_selectedMeasure]->setLength(length);
+	updateNoteState();
+}
+
+void Editor::channelUp() {
+	m_gui.text(L"channel").text = Format(++m_channel);
+}
+
+void Editor::channelDown() {
+	if (m_channel != 0)
+		m_gui.text(L"channel").text = Format(--m_channel);
 }
 
 Editor::Editor() :
@@ -68,27 +184,46 @@ Editor::Editor() :
 {
 	//GUI初期化
 
-	m_gui.addln(GUIText::Create(L"グリッド"));
+	m_gui.setTitle(L"メニュー");
+	m_gui.addln(GUIText::Create(L"[グリッド]"));
 	m_gui.add(GUIText::Create(L"ズーム"));
 	m_gui.add(L"zoom", GUISlider::Create(0.2, 2.0, 1.0));
-	m_gui.addln(L"zoom-val", GUIText::Create(L"100%"));
+	m_gui.addln(L"zoom-ratio", GUIText::Create(L"100%"));
 
-	m_gui.add(L"hr", GUIHorizontalLine::Create(1));
+	m_gui.add(GUIHorizontalLine::Create(1));
 
-	m_gui.addln(GUIText::Create(L"テンポ変更"));
-	m_gui.add(GUIText::Create(L"BPM"));
+	m_gui.addln(GUIText::Create(L"[ノート]"));
+	m_gui.add(GUIText::Create(L"チャンネル　"));
+	m_gui.add(L"channel-down", GUIButton::Create(L"-"));
+	m_gui.add(L"channel", GUIText::Create(L"0"));
+	m_gui.addln(L"channel-up", GUIButton::Create(L"+"));
+
+	m_gui.add(GUIHorizontalLine::Create(1));
+
+	m_gui.addln(GUIText::Create(L"[拡張ノート]"));
+	m_gui.add(GUIText::Create(L"テンポ変更　"));
 	m_gui.addln(L"tempo", GUITextField::Create(none));
 
-	m_gui.add(L"hr", GUIHorizontalLine::Create(1));
+	m_gui.add(GUIHorizontalLine::Create(1));
 
-	m_gui.addln(GUIText::Create(L"小節"));
+	m_gui.addln(GUIText::Create(L"[小節]"));
 	m_gui.add(L"add-measure", GUIButton::Create(L"追加"));
 	m_gui.addln(L"remove-measure", GUIButton::Create(L"削除"));
-	m_gui.add(L"tf1", GUITextField::Create(none));
+	m_gui.add(L"measure-length", GUITextField::Create(none));
 	m_gui.add(L"set-measure", GUIButton::Create(L"変更"));
+
+	m_gui.add(GUIHorizontalLine::Create(1));
+
+	m_gui.addln(GUIText::Create(L"[音源]"));
+	m_gui.add(GUIText::Create(L"基準テンポ　"));
+	m_gui.addln(L"measure-length", GUITextField::Create(none));
+	m_gui.add(GUIText::Create(L"オフセット　"));
+	m_gui.addln(L"measure-length", GUITextField::Create(none));
+
+
 	m_gui.setPos(m_rect.tr);
 
-	for (int i = 0; i < 200; ++i) {
+	for (size_t i = 0; i < MEASURE_DEFAULT; ++i) {
 		m_measures.push_back(std::make_shared<Measure>());
 	}
 }
@@ -96,16 +231,64 @@ Editor::Editor() :
 void Editor::update() {
 	//GUI関連
 	if (m_gui.slider(L"zoom").hasChanged) {
-		m_zoom = m_gui.slider(L"zoom").value;
-		m_gui.text(L"zoom-val").text = Format(static_cast<int>(m_zoom * 100), L"%");
-		updateNoteState();
+		changeZoomRatio();
+	}
+	else if (m_gui.button(L"add-measure").pushed) {
+		addMeasure();
+	}
+	else if (m_gui.button(L"remove-measure").pushed) {
+		removeMeasure();
+	}
+	else if (m_gui.button(L"set-measure").pushed) {
+		setMeasureLength();
+	}
+	else if (m_gui.button(L"channel-up").pushed) {
+		channelUp();
+	}
+	else if (m_gui.button(L"channel-down").pushed) {
+		channelDown();
 	}
 
 	//TODO:キーショートカット
+	if (Input::KeyControl.pressed) {
+		if (Input::KeyS.clicked) {
+			//TODO:Save
+		}
+		else if (Input::KeyZ.clicked) {
+			//TODO:Undo
+		}
+		else if (Input::KeyY.clicked) {
+			//TODO:Redo
+		}
+	}
 
-	if (m_rect.leftClicked) {
+	NoteType currentNoteType = NoteType::Tap;
 
-		Point mousePos = Mouse::Pos().movedBy(0, -Window::ClientRect().h);
+	//ノート選択
+	if (Input::KeyControl.pressed && Input::MouseL.clicked) {
+		currentNoteType = NoteType::Slide;
+	}
+	else if (Input::KeyShift.pressed) {
+		if (Input::MouseL.clicked) {
+			currentNoteType = NoteType::LFlick;
+		}
+		else if (Input::MouseR.clicked) {
+			currentNoteType = NoteType::RFlick;
+		}
+	}
+	else {
+		if (Input::MouseL.clicked) {
+			currentNoteType = NoteType::Tap;
+		}
+		else if (Input::MouseR.clicked) {
+			currentNoteType = NoteType::Long;
+		}
+	}
+
+
+	if (m_rect.leftClicked || m_rect.rightClicked) {
+
+		Point mousePos = Mouse::Pos().movedBy(0, -Window::ClientRect().h + m_offset);
 
 		int lane = 0;
 		int offset = getLaneOffset() - m_LaneWidth / 2;
@@ -128,7 +311,7 @@ void Editor::update() {
 			//(分割線が小節開始線のみ?小節の中間:小節開始線-グリッドの高さ/2)以上、小節開始線と前の小節の最後の分割線の中間未満
 			if (y - (lineCount == 1 ? (y - (*it)->EndY) / 2 : gridHeightHalf) <= mousePos.y && mousePos.y < y + (it == m_measures.begin() ? gridHeightHalf : ((*(it - 1))->LastLineY - y) / 2))
 			{
-				setOrRemoveNote((*it)->getNotes(), 0, lane);
+				setOrRemoveNote((*it)->getNotes(), 0, lane, currentNoteType);
 			}
 			y -= gridHeightHalf * 2;
 
@@ -140,7 +323,7 @@ void Editor::update() {
 				if (y - gridHeightHalf <= mousePos.y && mousePos.y < y + gridHeightHalf)
 				{
 					int tick = 192 / (m_division / 4) * i;
-					setOrRemoveNote((*it)->getNotes(), tick, lane);
+					setOrRemoveNote((*it)->getNotes(), tick, lane, currentNoteType);
 				}
 				y -= gridHeightHalf * 2;
 			}
@@ -150,7 +333,7 @@ void Editor::update() {
 			if (lineCount != 1 && y - (y - (*it)->EndY) / 2 <= mousePos.y && mousePos.y < y + gridHeightHalf)
 			{
 				int tick = 192 / (m_division / 4) * (lineCount - 1);
-				setOrRemoveNote((*it)->getNotes(), tick, lane);
+				setOrRemoveNote((*it)->getNotes(), tick, lane, currentNoteType);
 			}
 		}
 		updateNoteState();
@@ -170,7 +353,7 @@ void Editor::draw() {
 
 	Rect(5 * m_LaneWidth + getLaneOffset() + 20, Window::ClientRect().y, 1, Window::ClientRect().h).draw(Color(50, 50, 50));
 
-	Graphics2D::SetTransform(Mat3x2::Identity().translate(0, Window::ClientRect().h));
+	Graphics2D::SetTransform(Mat3x2::Identity().translate(0, Window::ClientRect().h - m_offset));
 
 	//小節
 	double measureBeginY = 0;
@@ -201,9 +384,13 @@ void Editor::draw() {
 		FontAsset(L"editor")(Pad(i, { 3, L'0' })).drawAt(getLaneOffset() - 70, m_measures[i]->BeginY);
 	}
 
+	//リボン
+	for (auto& note : m_playableNotes) {
+		note->drawRibbon();
+	}
+
 	//ノート
-	for (auto& note : m_notes) {
-		if (typeid(*note) == typeid(PlayableNote))
+	for (auto& note : m_playableNotes) {
 			note->draw();
 	}
 
--- DereEditor/Editor.h
+++ DereEditor/Editor.h
@@ -8,13 +8,19 @@ class Editor {
 private:
 	std::vector<std::shared_ptr<Measure>> m_measures;
 	std::vector<std::shared_ptr<Note>> m_notes;
+	std::vector<std::shared_ptr<PlayableNote>> m_playableNotes;
 	const int BeatPerHeight = 100;
 	const int m_LaneOffset = 125;
 	const int m_LaneWidth = 70;
-	const int m_division = 8;
+	int m_division = 8;
+	int m_channel = 0;
+	const int m_offset = 60;
+	const size_t MEASURE_MAX = 999;
+	const size_t MEASURE_MIN = 1;
+	const size_t MEASURE_DEFAULT = 5;
 	double m_zoom = 1.0;
 	const Rect m_rect;
-	int m_selectedMeasure = 1;
+	size_t m_selectedMeasure = 1;
 	GUI m_gui;
 	Rect m_selectedRect;
 	Point m_selectBegin;
@@ -29,7 +35,17 @@ private:
 
 	int getLaneOffset();
 
-	void setOrRemoveNote(std::vector<std::shared_ptr<Note>> &notes, int tick, int lane);
+	void setOrRemoveNote(std::vector<std::shared_ptr<Note>> &notes, int tick, int lane, NoteType type);
+
+	void changeZoomRatio();
+
+	void channelUp();
+	void channelDown();
+
+	void addMeasure();
+	void removeMeasure();
+	void setMeasureLength();
+
 
 public:
 
