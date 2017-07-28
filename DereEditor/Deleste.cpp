5bfc38966711bf05d517a9cc10fa646a2a3bf881
rare <work@DESKTOP-Q6BN0UU>
2017-07-29 02:32:32 +0900

    Deleste形式の準備

---
+++ DereEditor/Deleste.cpp
@@ -0,0 +1,6 @@
+#include "Deleste.h"
+
+Deleste::Deleste(String path) {
+
+}
+
---
+++ DereEditor/Deleste.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Note.h"
+
+class Deleste
+{
+public:
+	Deleste(String path);
+	Deleste() = default;
+	~Deleste() = default;
+};
+
